dd97ac7a90912779205933386d3744bd37a70937
Luke Catherall <catherluke@ecs.vuw.ac.nz>
2021-05-25 16:02:47 +1200

    Set Up Imports and Methods

--- src/main/java/nz/ac/wgtn/swen301/a3/server/LogsServlet.java
+++ src/main/java/nz/ac/wgtn/swen301/a3/server/LogsServlet.java
@@ -1,20 +1,27 @@
 package nz.ac.wgtn.swen301.a3.server;
 
-public class LogsServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LogsServlet extends HttpServlet {
 
     public LogsServlet() {
 
     }
 
-    public void doGet() {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
     }
 
-    public void doPost() {
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
     }
 
-    public void doDelete() {
+    public void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
     }
 

dbb950fbaf629000aa137056c5e1a3d90a36b3f3
Luke Catherall <catherluke@ecs.vuw.ac.nz>
2021-05-25 15:40:31 +1200

    Set Up Files

---
+++ src/main/java/nz/ac/wgtn/swen301/a3/server/LogsServlet.java
@@ -0,0 +1,21 @@
+package nz.ac.wgtn.swen301.a3.server;
+
+public class LogsServlet {
+
+    public LogsServlet() {
+
+    }
+
+    public void doGet() {
+
+    }
+
+    public void doPost() {
+
+    }
+
+    public void doDelete() {
+
+    }
+
+}

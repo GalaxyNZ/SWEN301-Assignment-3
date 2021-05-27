da2c4cd5afdd4fc302cbb3985b4b0e42cdf55055
Luke Catherall <catherluke@ecs.vuw.ac.nz>
2021-05-27 16:02:33 +1200

    Check for bad input in LogsServlet::doGet

--- src/main/java/nz/ac/wgtn/swen301/a3/server/LogsServlet.java
+++ src/main/java/nz/ac/wgtn/swen301/a3/server/LogsServlet.java
@@ -5,6 +5,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InvalidObjectException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class LogsServlet extends HttpServlet {
 
@@ -14,7 +18,24 @@ public class LogsServlet extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+
+        String level = req.getParameter("level");
+        if (level == null) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+        int limit;
+        try {
+            limit = Integer.parseInt(req.getParameter("limit"));
+        } catch (NumberFormatException e) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        resp.setContentType("application/json");
+
+
+        //super.doGet(req, resp);
     }
 
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
--- src/main/java/nz/ac/wgtn/swen301/a3/server/Persistency.java
+++ src/main/java/nz/ac/wgtn/swen301/a3/server/Persistency.java
@@ -3,11 +3,11 @@ package nz.ac.wgtn.swen301.a3.server;
 import java.util.ArrayList;
 import java.util.List;
 
-//import com.fasterxml.jackson.*;
+import com.fasterxml.jackson.databind.JsonNode;
 
 public class Persistency {
 
-    public static List<String> DB = new ArrayList<>();
+    public static List<JsonNode> DB = new ArrayList<>();
 
     public Persistency() {}
 
